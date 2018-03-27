9b9977b0e4d50683ce5eb2aed075516a99d16b50
Jason Irish <jsirish@users.noreply.github.com>
2018-03-27 10:14:04 -0500

    Sponsor - add can permissions (#3)
    closes dynamic/para-world-sailing-installer#34

--- tests/Model/SponsorTest.php
+++ tests/Model/SponsorTest.php
@@ -5,6 +5,7 @@ namespace Dynamic\Elements\Tests\Model;
 use Dynamic\Elements\Model\Sponsor;
 use Sheadawson\Linkable\Forms\LinkField;
 use SilverStripe\Dev\SapphireTest;
+use SilverStripe\Security\Member;
 
 /**
  * Class SponsorTest
@@ -24,5 +25,73 @@ class SponsorTest extends SapphireTest
     {
         $sponsor = Sponsor::singleton();
         $this->assertInstanceOf(LinkField::class, $sponsor->getCMSFields()->dataFieldByName('LinkID'));
+    }
+
+    /**
+     *
+     */
+    public function testCanView()
+    {
+        $object = $this->objFromFixture(Sponsor::class, 'one');
+
+        $admin = $this->objFromFixture(Member::class, 'admin');
+        $this->assertTrue($object->canView($admin));
+
+        $siteowner = $this->objFromFixture(Member::class, 'site-owner');
+        $this->assertTrue($object->canView($siteowner));
+
+        $member = $this->objFromFixture(Member::class, 'default');
+        $this->assertNull($object->canView($member));
+    }
+
+    /**
+     *
+     */
+    public function testCanEdit()
+    {
+        $object = $this->objFromFixture(Sponsor::class, 'one');
+
+        $admin = $this->objFromFixture(Member::class, 'admin');
+        $this->assertTrue($object->canEdit($admin));
+
+        $siteowner = $this->objFromFixture(Member::class, 'site-owner');
+        $this->assertTrue($object->canEdit($siteowner));
+
+        $member = $this->objFromFixture(Member::class, 'default');
+        $this->assertNull($object->canEdit($member));
+    }
+
+    /**
+     *
+     */
+    public function testCanDelete()
+    {
+        $object = $this->objFromFixture(Sponsor::class, 'one');
+
+        $admin = $this->objFromFixture(Member::class, 'admin');
+        $this->assertTrue($object->canDelete($admin));
+
+        $siteowner = $this->objFromFixture(Member::class, 'site-owner');
+        $this->assertTrue($object->canDelete($siteowner));
+
+        $member = $this->objFromFixture(Member::class, 'default');
+        $this->assertNull($object->canDelete($member));
+    }
+
+    /**
+     *
+     */
+    public function testCanCreate()
+    {
+        $object = $this->objFromFixture(Sponsor::class, 'one');
+
+        $admin = $this->objFromFixture(Member::class, 'admin');
+        $this->assertTrue($object->canCreate($admin));
+
+        $siteowner = $this->objFromFixture(Member::class, 'site-owner');
+        $this->assertTrue($object->canCreate($siteowner));
+
+        $member = $this->objFromFixture(Member::class, 'default');
+        $this->assertNull($object->canCreate($member));
     }
 }
